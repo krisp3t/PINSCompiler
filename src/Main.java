ff315af3b2442208dc6fc20b8ca2c6b1135cf225
krisp3t <77499501+krisp3t@users.noreply.github.com>
2023-03-11 20:46:06 +0100

    Initial

---
+++ src/Main.java
@@ -0,0 +1,62 @@
+/**
+ * @Author: turk
+ * @Description: Vhodna točka prevajalnika.
+ */
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Optional;
+
+import cli.PINS;
+import cli.PINS.Phase;
+import compiler.lexer.Lexer;
+import compiler.parser.Parser;
+
+public class Main {
+    /**
+     * Metoda, ki izvede celotni proces prevajanja.
+     * 
+     * @param args parametri ukazne vrstice.
+     */
+    public static void main(String[] args) throws Exception {
+        var cli = PINS.parse(args);
+        run(cli);
+    }
+
+
+    // -------------------------------------------------------------------
+
+
+    private static void run(PINS cli) throws IOException {
+        var sourceCode = Files.readString(Paths.get(cli.sourceFile));
+        run(cli, sourceCode);
+    }
+
+    private static void run(PINS cli, String sourceCode) {
+        /**
+         * Izvedi leksikalno analizo.
+         */
+        var symbols = new Lexer(sourceCode).scan();
+        if (cli.dumpPhases.contains(Phase.LEX)) {
+            for (var symbol : symbols) {
+                System.out.println(symbol.toString());
+            }
+        }
+        if (cli.execPhase == Phase.LEX) {
+            return;
+        }
+        /**
+         * Izvedi sintaksno analizo.
+         */
+        Optional<PrintStream> out = cli.dumpPhases.contains(Phase.SYN) 
+                ? Optional.of(System.out)
+                : Optional.empty();
+        var parser = new Parser(symbols, out);
+        parser.parse();
+        if (cli.execPhase == Phase.SYN) {
+            return;
+        }
+    }
+}
