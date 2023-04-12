20e868124e15eedcd2e1a8bf9817b5f6db6a1e1a
krisp3t <77499501+krisp3t@users.noreply.github.com>
2023-04-12 12:18:25 +0200

    Checkpoint

--- src/compiler/seman/type/TypeChecker.java
+++ src/compiler/seman/type/TypeChecker.java
@@ -36,108 +36,253 @@ public class TypeChecker implements Visitor {
     @Override
     public void visit(Call call) {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit'");
+        for (Expr argument : call.arguments)
+            argument.accept(this);
     }
 
     @Override
     public void visit(Binary binary) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit'");
+        binary.left.accept(this);
+        binary.right.accept(this);
+
+        if (!(types.valueFor(binary.left).isPresent() && types.valueFor(binary.right).isPresent()))
+            Report.error(binary.position, "Tipov v binary expressionu ni bilo mogoče določiti");
+
+        // { expr1 = expr2 }
+        if (binary.operator.equals(Binary.Operator.EQ)) {
+            Type t1 = types.valueFor(binary.left).get();
+            Type t2 = types.valueFor(binary.right).get();
+            if (!t1.equals(t2))
+                Report.error(binary.position, "Tipa v binary expressionu morata biti enaka!");
+            // return type t1==t2
+            types.store(t1, binary);
+            return;
+        }
+
+        // &, |
+        if (binary.operator.isAndOr()) {
+            Type t1 = types.valueFor(binary.left).get();
+            Type t2 = types.valueFor(binary.right).get();
+            if (!(t1.isLog() && t2.isLog()))
+                Report.error(binary.position, "Pričakovan tip v AND/OR izrazu je LOGICAL!");
+
+            // return type LOGICAL
+            types.store(new Type.Atom(Type.Atom.Kind.LOG), binary);
+        }
+
+        // +, -, *, /, %
+        if (binary.operator.isArithmetic()) {
+            Expr[] nodes = {binary.left, binary.right};
+            for (Expr node : nodes) {
+                Type t = types.valueFor(node).get();
+                if (!t.isInt())
+                    Report.error(node.position, "Pričakovan tip v aritmetičnem izrazu je INTEGER!");
+            }
+            // return type INTEGER
+            types.store(new Type.Atom(Type.Atom.Kind.INT), binary);
+        }
+
+        // ==, !=, <=, >=, <, >
+        if (binary.operator.isComparison()) {
+            Type t1 = types.valueFor(binary.left).get();
+            Type t2 = types.valueFor(binary.right).get();
+            if (!t1.equals(t2))
+                Report.error(binary.position, "Tipa v binary expressionu morata biti enaka!");
+            if (!(t1.isInt() || t1.isLog()) || !(t2.isInt() || t2.isLog()))
+                Report.error(binary.position, "Pričakovan tip v primerjalnem izrazu je INTEGER ali LOGICAL!");
+
+            // return type LOGICAL
+            types.store(new Type.Atom(Type.Atom.Kind.LOG), binary);
+        }
     }
 
     @Override
     public void visit(Block block) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit'");
+        for (Expr expr : block.expressions) {
+            expr.accept(this);
+        }
+        if (types.valueFor(block.expressions.get(block.expressions.toArray().length - 1)).isPresent()) {
+            // return type zadnji expr
+            Type t = types.valueFor(block.expressions.get(block.expressions.toArray().length - 1)).get();
+            types.store(t, block);
+        } else {
+            Report.error(block.position, "Tipa bloka ni bilo mogoče določiti");
+        }
     }
 
     @Override
     public void visit(For forLoop) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit'");
+        forLoop.counter.accept(this);
+        forLoop.low.accept(this);
+        forLoop.high.accept(this);
+        forLoop.step.accept(this);
+        forLoop.body.accept(this);
+
+        Expr[] nodes = {forLoop.low, forLoop.high, forLoop.step};
+        for (Expr node : nodes) {
+            if (types.valueFor(node).isPresent()) {
+                Type t = types.valueFor(node).get();
+                if (!t.isInt())
+                    Report.error(node.position, "Pričakovan tip v for loopu je INTEGER!");
+            } else {
+                Report.error(node.position, "Tipa v for loopu ni bilo mogoče določiti");
+            }
+        }
+
+        // return type VOID
+        types.store(new Type.Atom(Type.Atom.Kind.VOID), forLoop);
     }
 
     @Override
     public void visit(Name name) {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit'");
     }
 
     @Override
     public void visit(IfThenElse ifThenElse) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit'");
+        ifThenElse.condition.accept(this);
+        ifThenElse.thenExpression.accept(this);
+        ifThenElse.elseExpression.ifPresent(expr -> expr.accept(this));
+
+        if (types.valueFor(ifThenElse.condition).isPresent()) {
+            Type t = types.valueFor(ifThenElse.condition).get();
+            if (!t.isLog())
+                Report.error(ifThenElse.condition.position, "Pričakovan tip v if stavku je LOGICAL!");
+        } else {
+            Report.error(ifThenElse.condition.position, "Tipa v if stavku ni bilo mogoče določiti");
+        }
+
+        // return type VOID
+        types.store(new Type.Atom(Type.Atom.Kind.VOID), ifThenElse);
     }
 
     @Override
     public void visit(Literal literal) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit'");
+        Type.Atom.Kind kind = switch (literal.type) {
+            case INT -> Type.Atom.Kind.INT;
+            case LOG -> Type.Atom.Kind.LOG;
+            case STR -> Type.Atom.Kind.STR;
+        };
+        this.types.store(new Type.Atom(kind), literal);
     }
 
     @Override
     public void visit(Unary unary) {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit'");
+        
     }
 
     @Override
     public void visit(While whileLoop) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit'");
+        whileLoop.condition.accept(this);
+        whileLoop.body.accept(this);
+
+        if (types.valueFor(whileLoop.condition).isPresent()) {
+            Type t = types.valueFor(whileLoop.condition).get();
+            if (!t.isLog())
+                Report.error(whileLoop.condition.position, "Pričakovan tip v while stavku je LOGICAL!");
+        } else {
+            Report.error(whileLoop.condition.position, "Tipa v while stavku ni bilo mogoče določiti");
+        }
+
+        // return type VOID
+        types.store(new Type.Atom(Type.Atom.Kind.VOID), whileLoop);
     }
 
     @Override
     public void visit(Where where) {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit'");
+        where.defs.accept(this); // 2 obhoda v Defs
+        where.expr.accept(this);
     }
 
     @Override
     public void visit(Defs defs) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit'");
+        for (Def def : defs.definitions) {
+            def.accept(this);
+        }
     }
 
     @Override
     public void visit(FunDef funDef) {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit'");
+        // 1. obhod
+        // return type
+        funDef.type.accept(this);
+        // type parametrov
+        for (Parameter parameter : funDef.parameters) {
+            parameter.type.accept(this);
+        }
+        // imena parametrov
+        for (Parameter parameter : funDef.parameters) {
+            parameter.accept(this);
+        }
+        // expression
+        funDef.body.accept(this);
     }
 
     @Override
     public void visit(TypeDef typeDef) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit'");
+        typeDef.type.accept(this);
+
+        // TODO: preveri, če typ obstaja
+        if (types.valueFor(typeDef.type).isEmpty())
+            Report.error(typeDef.position, "Tip " + typeDef.type + "ne obstaja!");
+
+        Type t = types.valueFor(typeDef.type).get();
+        types.store(t, typeDef);
     }
 
     @Override
     public void visit(VarDef varDef) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit'");
+        varDef.type.accept(this);
+
+        // TODO: preveri, če typ obstaja
+        if (types.valueFor(varDef.type).isEmpty())
+            Report.error(varDef.position, "Tip " + varDef.type + "ne obstaja!");
+
+        Type t = types.valueFor(varDef.type).get();
+        types.store(t, varDef);
+
     }
 
     @Override
     public void visit(Parameter parameter) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit'");
+        parameter.type.accept(this);
+
+        // TODO: preveri, če typ obstaja
+        if (types.valueFor(parameter.type).isEmpty())
+            Report.error(parameter.position, "Tip " + parameter.type + "ne obstaja!");
+
+        Type t = types.valueFor(parameter.type).get();
+        types.store(t, parameter);
     }
 
     @Override
     public void visit(Array array) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit'");
+        array.type.accept(this);
+
+        // TODO: preveri, če typ obstaja
+        if (types.valueFor(array.type).isEmpty())
+            Report.error(array.position, "Tip " + array.type + "ne obstaja!");
+
+        Type t = types.valueFor(array.type).get();
+        types.store(t, array);
     }
 
     @Override
     public void visit(Atom atom) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit'");
+        Type.Atom.Kind kind = switch (atom.type) {
+            case INT -> Type.Atom.Kind.INT;
+            case LOG -> Type.Atom.Kind.LOG;
+            case STR -> Type.Atom.Kind.STR;
+        };
+        this.types.store(new Type.Atom(kind), atom);
     }
 
     @Override
     public void visit(TypeName name) {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'visit'");
+        System.out.println("");
     }
 }
